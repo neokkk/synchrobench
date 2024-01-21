298868589f7a9a1ad77b421cab1f54afb0f1140b
neokkk <seven3126@gmail.com>
2024-01-21 23:55:17 +0900

    feat: benchmark for thread_count

--- c-cpp/scripts/nhs_skiplist/thread_count.sh
+++ c-cpp/scripts/nhs_skiplist/thread_count.sh
@@ -11,21 +11,21 @@
 # Select appropriate parameters below
 #
 #threads="1 4 8 12 16 20 24 28 32"
-threads="1 20 40 100"
+threads="1 4 16 24"
 #benchs="tcmalloc-lockfree-ll tcmalloc-spinlock-ht tcmalloc-estm-rt tcmalloc-estm-sl tcmalloc-fraser-sl tcmalloc-rotating-sl tcmalloc-spinlock-ll tcmalloc-estm-ll tcmalloc-estm-st tcmalloc-lockfree-ht tcmalloc-spinlock-sl tcmalloc-estm-ht tcmalloc-spinlock-btree"
 benchs="lockfree-nohotspot-skiplist"
 #lockfree-fraser-skiplist lockfree-rotating-skiplist lockfree-nohotspot-skiplist SPIN-skiplist"
 #seqbenchs="tcmalloc-sequential-ll tcmalloc-sequential-rt tcmalloc-sequential-ht tcmalloc-sequential-sl"
 seqbenchs=""
+#iterations="1 2 3 4 5 6 7 8 9 10"
 iterations="1"
-# iterations=""
-# for i in `seq 1 1 5`; do # 1..5 with step 1
-#   iterations="$iterations $i"
-# done
 #updates="0 100"
-updates="20"
+updates="10 50 100"
 #size="1024 4096 8192 16384 32768 65536"
-sizes="65536"
+sizes="2097152"
+# sizes="64 65536 67108864"
+#range=""
+ranges="128 1280000 128000000"
 #deqbenchs="estm-dq tcmalloc-estm-dq tcmalloc-sequential-dq"
 deqbenchs=""
 ###
@@ -43,54 +43,19 @@ fi
 
 for size in ${sizes}; do
   # make the range twice as large as initial size to maintain size expectation 
-  r=`echo "2*${size}" | bc`
-  for upd in ${updates}; do 
-    for thread in ${threads}; do
-      for iter in ${iterations}; do
-        for bench in ${benchs}; do 
-          ${bin}/${bench} -u ${upd} -i ${size} -r ${r} -d 5000 -t ${thread} -f 0 > ./log/${bench}-n${thread}-i${size}-u${upd}.${iter}.log
+  for range in ${ranges}; do
+    if [ ${range} -lt ${size} ]; then
+      continue
+    fi
+    for upd in ${updates}; do
+      for thread in ${threads}; do
+        for iter in ${iterations}; do
+          for bench in ${benchs}; do 
+            ${bin}/${bench} -u ${upd} -i ${size} -r ${range} -d 5000 -t ${thread} -f 0 > ./log/${bench}-n${thread}-r${range}-i${size}-u${upd}.${iter}.log
+          done
+          echo "Done experimenting concurrent benchs for 5000 milliseconds each"
         done
-        echo "Done experimenting concurrent benchs for 5000 milliseconds each"
       done
     done
   done
-done
-
-# for sequential
-for size in ${sizes}; do
-  r=`echo "2*${size}" | bc`
-  for upd in ${updates}; do 
-    for iter in ${iterations}; do
-      for bench in ${seqbenchs}; do 
-        ${bin}/${bench} -u ${upd} -i ${size} -r ${r} -d 5000 -t 1 -f 0 > ./log/${bench}-i${size}-u${upd}-sequential.${iter}.log
-      done
-    done
-  done
-done
-
-# for dequeue
-for upd in ${updates}; do 
-  for thread in ${threads}; do
-    for iter in ${iterations}; do
-      for bench in ${deqbenchs}; do
-      ${bin}/${bench} -i 128 -r 256 -d 5000 -t ${thread} > ./log/${bench}-n${thread}.${iter}.log
-      done
-    done
-  done
-done
-
-# for the lock-coupling linked list
-#benchs="tcmalloc-spinlock-ll tcmalloc-spinlock-ht"
-benchs=""
-for size in ${sizes}; do
- r=`echo "2*${size}" | bc`
- for upd in ${updates}; do 
-  for thread in ${threads}; do
-   for iter in ${iterations}; do
-    for bench in ${benchs}; do 
-      ${bin}/${bench} -x2 -u ${upd} -i ${size} -r ${r} -d 5000 -t ${thread} -f 0 > ./log/${bench}-n${thread}-i${size}-u${upd}-lazy.${iter}.log
-    done
-   done
-  done
- done
 done
